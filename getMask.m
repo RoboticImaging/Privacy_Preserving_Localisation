1f74163611755290e0d7e8fa0598bd7f82025c86
Adam T <ataras2@gmail.com>
2022-01-20 11:21:35 +1100

    added hardware mask function

--- getMask.m
+++
@@ -1,4 +0,0 @@
-function m = getMask(size)
-    m.size = size;
-    m.values = zeros(size,'uint8');
-end
---
+++ getValidHardwareMask.m
@@ -0,0 +1,35 @@
+function mask = getValidHardwareMask(maskName, opt)
+    % restrict fspecial to only use non negative values for valid hardware
+    % implementations
+    if strcmp(maskName, "disk")
+        mask = fspecial('disk',opt.radius);
+        
+    elseif strcmp(maskName, "gaussian")
+        mask = fspecial('gaussian',opt.hsize,opt.sigma);
+        
+    elseif strcmp(maskName, "average")
+        mask = fspecial('average',opt.hsize);
+        
+    elseif strcmp(maskName, "edge")
+        mask = zeros(opt.hsize);
+        
+        % opt.edgeStrength>1 is the ratio of the two halves of the filter
+        
+        % make v by default
+        % check if middle row is going to be middle
+        if mod(size(mask,1),2) == 0
+            mask(:,1:size(mask,1)/2) = opt.edgeStrength;
+            mask(:,size(mask,1)/2 + 1:end) = 1;
+        else
+            mask(:,1:(size(mask,1)-1)/2) = opt.edgeStrength;
+            mask(:,(size(mask,1)+1)/2 + 1:end) = 1;
+            mask(:,(size(mask,1)+1)/2) = mean([opt.edgeStrength,1]);
+        end
+        
+        mask = mask./sum(mask(:));
+        
+        if opt.isVertical
+            mask = mask';
+        end
+    end
+end
